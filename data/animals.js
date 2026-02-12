5b64a4689ed7e1bb6c8048e4a03e7296ac92f29b
Arash <Safari.arash@gmail.com>
2026-02-12 19:54:03 +0100

    add all animal data

---
+++ data/animals.js
@@ -0,0 +1,115 @@
+const animals = [
+  // --- MAMMALS ---
+  {
+    id: "echidna",
+    name: "Echidna",
+    group: "Mammals",
+    lifespan: "50 years",
+    food: "Insects such as ants and termites, beetle larvae and worms",
+    found: "Throughout Australia",
+    length: "76 cm",
+    weight: "10 kg",
+    description:
+      "Echidnas are monotremes, egg-laying mammals. Their bodies are covered with hollow, barbless quills.",
+  },
+  {
+    id: "tasmanian-devil",
+    name: "Tasmanian Devil",
+    group: "Mammals",
+    lifespan: "5 years",
+    food: "Meat from other animals such as wallabies and wombats",
+    found: "Tasmania",
+    length: "70 cm",
+    weight: "10 kg",
+    description:
+      "The world's largest carnivorous marsupial, known for its powerful bite and disturbing screech.",
+  },
+  {
+    id: "quokka",
+    name: "Quokka",
+    group: "Mammals",
+    lifespan: "10 years",
+    food: "Plant eaters, they munch on shrubs and grasses",
+    found: "Rottnest Island and mainland Western Australia",
+    length: "50 cm",
+    weight: "3 kg",
+    description:
+      "Known as 'the world's happiest animal', it looks like a small wallaby with coarse grey-brown fur.",
+  },
+  // --- REPTILES ---
+  {
+    id: "frill-necked-lizard",
+    name: "Frill-necked Lizard",
+    group: "Reptiles",
+    lifespan: "20 years",
+    food: "Small insects and spiders",
+    found: "Northern Australia",
+    length: "90 cm",
+    weight: "1 kg",
+    description:
+      "Unique for the colorful pleated skin flap around its head that unfurls when it feels threatened.",
+  },
+  {
+    id: "hawksbill-turtle",
+    name: "Hawksbill Turtle",
+    group: "Reptiles",
+    lifespan: "50 years",
+    food: "Sponges, jellyfish, and sea plants",
+    found: "Tropical coasts of Australia",
+    length: "80 cm",
+    weight: "50 kg",
+    description:
+      "Named for its beak-like mouth. Its shell has a serrated edge and beautiful overlapping scales.",
+  },
+  {
+    id: "perentie",
+    name: "Perentie",
+    group: "Reptiles",
+    lifespan: "20 years",
+    food: "Carnivore (kangaroos, rabbits, lizards and birds)",
+    found: "Arid areas (Deserts) of Australia",
+    length: "250 cm",
+    weight: "20 kg",
+    description:
+      "The largest monitor lizard native to Australia and the world's fourth largest living lizard.",
+  },
+  // --- BIRDS ---
+  {
+    id: "cassowary",
+    name: "Cassowary",
+    group: "Birds",
+    lifespan: "20 years",
+    food: "Fruit, insects and small animals",
+    found: "Queensland",
+    length: "170 cm",
+    weight: "44 kg",
+    description:
+      "A large flightless bird with powerful legs, capable of moving quickly on land and water.",
+  },
+  {
+    id: "kookaburra",
+    name: "Kookaburra",
+    group: "Birds",
+    lifespan: "20 years",
+    food: "Insects, snakes, frogs and lizards",
+    found: "Australia wide",
+    length: "43 cm",
+    weight: "0.3 kg",
+    description:
+      "The largest member of the Kingfisher family, famous for its distinctive laughing call.",
+  },
+  {
+    id: "yellow-tailed-black-cockatoo",
+    name: "Yellow Tailed Black Cockatoo",
+    group: "Birds",
+    lifespan: "41 years",
+    food: "Fruit, seeds and other plant material",
+    found: "South Eastern Australia",
+    length: "65 cm",
+    weight: "0.9 kg",
+    description:
+      "A large cockatoo with brownish-black plumage and prominent yellow cheek patches.",
+  },
+];
+
+export default animals;
