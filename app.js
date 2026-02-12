f7b11ce1592338ed0ebf0897fa8cb66f88d60df6
Arash <Safari.arash@gmail.com>
2026-02-12 18:42:38 +0100

    Initial commit: setting up project structure

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import * as path from "path";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT;
+
+const __dirname = path.resolve();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
