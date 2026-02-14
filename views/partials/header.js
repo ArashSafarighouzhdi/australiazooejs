411d4c0fd9f526b9ffc281e09bd071245bd6e4d7
Arash <Safari.arash@gmail.com>
2026-02-14 11:06:37 +0100

    Add  partials

---
+++ views/partials/header.js
@@ -0,0 +1,26 @@
+<header>
+  <nav>
+    <ul class="menu">
+      <li>
+        <a href="/" class="menu-link">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/group/mammals" class="menu-link">
+          Mammals
+        </a>
+      </li>
+      <li>
+        <a href="/group/birds" class="menu-link">
+          Birds
+        </a>
+      </li>
+      <li>
+        <a href="/group/reptiles" class="menu-link">
+          Reptiles
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>;
