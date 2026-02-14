b134b974aaad96bd55254acbc9353be8f2391f56
Arash <Safari.arash@gmail.com>
2026-02-14 19:40:23 +0100

    Add animal.ejs and group.ejs

--- routes/groupRouter.js
+++ routes/groupRouter.js
@@ -1,14 +1,26 @@
-import express from "express"
-import * as path from "path"
+import express from "express";
+import * as path from "path";
+import animals from "../data/animals.js";
 
-const groupRouter = express.Router()
-const __dirname = path.resolve()
-
+const groupRouter = express.Router();
+const __dirname = path.resolve();
 
 groupRouter.get("/group/:category", (req, res) => {
+  const category = req.params.category;
+  const filteredAnimals = animals.filter(
+    (a) => a.group.toLowerCase() === category.toLowerCase(),
+  );
 
-   res.render(
-    path.join(__dirname, "/views/pages/group"))
-})
+  filteredAnimals.forEach((a) => {
+    a.image = `/images/${category}/${a.id}.png`;
+  });
 
-export default groupRouter 
+  res.render(path.join(__dirname, "/views/pages/group"), {
+    allAnimals: filteredAnimals,
+    welcomeTitle: category.charAt(0).toUpperCase() + category.slice(1),
+    subTitle: `Explore our amazing collection of ${category}.`,
+    documentTitle: category,
+  });
+});
+
+export default groupRouter;
