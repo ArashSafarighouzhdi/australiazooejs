e60bec5bba27d12017a74e81e0cef5a2ad2e3f31
Arash <Safari.arash@gmail.com>
2026-02-14 14:06:24 +0100

    fix error

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 import express from "express";
-import animalRouter from "./routes/animalRoutes.js";
 import * as path from "path";
 import "dotenv/config";
 import groupRouter from "./routes/groupRouter.js";
+import animalRouter from "./routes/animalRoutes.js";
+import animals from "./data/animals.js";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT;
-
 const __dirname = path.resolve();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-      res.render(
-        path.join(__dirname, "/views/pages/home"), 
-        {   
-          welcomeTitle: "Welcome Home Page"
-        }
-    )
+  res.render(path.join(__dirname, "/views/pages/home"), {
+    welcomeTitle: "Welcome Home Page",
+    subTitle: "Explore the amazing wildlife of Australia.",
+    documentTitle: "Australia Zoo | Home",
+    allAnimals: animals,
+  });
 });
 
-app.use("/", animalRouter)
+app.use("/", animalRouter);
 app.use("/", groupRouter);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
