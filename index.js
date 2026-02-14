bbdd11b6b28320018b48cc8d8b256774a2ec8203
Aleksandra Ristovska <aleksandra@ristovska.com>
2026-02-14 08:43:29 +0100

    home page

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Server is up and running!");
+      res.render(
+        path.join(__dirname, "/views/pages/home"), 
+        {   
+          welcomeTitle: "Welcome Home Page"
+        }
+    )
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
